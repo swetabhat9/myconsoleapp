0e2123522f4e0dc99868ba489ad0cb4a9bb1d176
swetabhat <swetabhat9@gmail.com>
2019-02-27 15:26:24 +0530

    feature1.cs file is added and in program.cs  add function is called

--- myapp/myapp/Program.cs
+++ myapp/myapp/Program.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using myapp;
 
 namespace myapp
 {
+   
     class Program
     {
+       
         static void Main(string[] args)
         {
+            Program p = new Program();
             //Here goes the logic to do some stuff
+
+            //here goes the logic for feature 1
+            int sum=feature1. add();
+            Console.WriteLine(sum);
+
+
         }
     }
 }
---
+++ myapp/myapp/feature1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myapp
+{
+    static class feature1
+    {
+       public static int add()
+        {
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
