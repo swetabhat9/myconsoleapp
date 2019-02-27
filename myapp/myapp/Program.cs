26bd10514ab3d4ed7fbef962422bc840594ef739
swetabhat <swetabhat9@gmail.com>
2019-02-27 15:39:00 +0530

    made changes in feature1

--- myapp/myapp/Program.cs
+++ myapp/myapp/Program.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using myapp;
 
 namespace myapp
 {
-   
     class Program
     {
-       
         static void Main(string[] args)
         {
-            Program p = new Program();
             //Here goes the logic to do some stuff
 
             //here goes the logic for feature 1---i have made this function parametrised
